d00cc63ef6be15e9150ffc453cfb737db8942356
komputer Siedlarz <zibi305@interia.pl>
2020-04-27 11:47:56 +0200

    Move to coroutines

--- AirQuality/app/src/main/java/com/example/airquality/MainActivity.kt
+++ AirQuality/app/src/main/java/com/example/airquality/MainActivity.kt
@@ -3,23 +3,25 @@ package com.example.airquality
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.EditText
-import android.widget.ListView
+import androidx.appcompat.app.AlertDialog
 import com.example.apiclient.APIClient
 import com.example.models.Station
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    var list: ListView? = null
-    var adapter: StationAdapter? = null
-    private var search: EditText? = null
-    private val apiClient: APIClient = APIClient(this)
+    private val apiClient: APIClient = APIClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,29 +32,56 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "By Sebastian Siedlarz", Snackbar.LENGTH_LONG).show()
         }
 
-        adapter = StationAdapter(this, arrayListOf())
-        list = findViewById(R.id.stationList)
-        search = findViewById(R.id.search)
+        var adapter = StationAdapter(this, arrayListOf())
 
-        search?.addTextChangedListener(object: TextWatcher{
+        search.addTextChangedListener(object: TextWatcher{
             override fun afterTextChanged(p0: Editable?) { }
 
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
 
             override fun onTextChanged(search: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                adapter?.filter(search)
+                adapter.filter(search)
             }
         })
 
         //onclick with popup
-        list?.setOnItemClickListener{
+        stationList.setOnItemClickListener{
                 parent, _, position, _ ->
             val station: Station = parent.getItemAtPosition(position) as Station
-            apiClient.getStationIndex(station.Id)
+            val index: Deferred<String?> = GlobalScope.async { apiClient.getStationIndex(station.Id) }
+            val builder = AlertDialog.Builder(this)
+            Log.d("asd", "ASd")
+            GlobalScope.launch {
+                val stationIndex = apiClient.getStationIndexData(index.await())
+                builder.setTitle("Data: " + stationIndex.Date)
+                builder.setMessage("Indeks jakości: " + stationIndex.Index)
+
+                runOnUiThread{
+                    builder.setIcon(R.drawable.ic_info_outline_black_24dp)
+                    builder.setPositiveButton("OK") { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    builder.show()
+                }
+            }
         }
 
         //Receiver list on screen
-        apiClient.getAllStation()
+
+        val listItems: ArrayList<Station> = arrayListOf()
+        val stations: Deferred<String?> = GlobalScope.async { apiClient.getAllStation() }
+        GlobalScope.launch{
+            for(item in apiClient.getAllStationList(stations.await())){
+                listItems.add(item)
+            }
+            runOnUiThread{
+                listItems.sort()
+                stationList.adapter = adapter
+            }
+        }
+
+        adapter = StationAdapter(this, listItems)
+        stationList.adapter = adapter
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- AirQuality/app/src/main/java/com/example/apiclient/APiClient.kt
+++ AirQuality/app/src/main/java/com/example/apiclient/APiClient.kt
@@ -1,56 +1,32 @@
 package com.example.apiclient
 
-import android.app.AlertDialog
-import android.content.DialogInterface
-import android.graphics.drawable.Drawable
-import android.util.Log
-import com.example.airquality.MainActivity
-import com.example.airquality.R
-import com.example.airquality.StationAdapter
 import com.example.models.Station
 import com.example.models.StationIndex
 import okhttp3.*
 import org.json.JSONArray
 import org.json.JSONObject
-import java.io.IOException
 
-class APIClient(context: MainActivity){
+class APIClient{
 
     private val urlStation: String = "https://api.gios.gov.pl/pjp-api/rest/station/findAll"
     private val urlStationIndex: String = "https://api.gios.gov.pl/pjp-api/rest/aqindex/getIndex/"
     private val client = OkHttpClient()
-    private var context: MainActivity = context
 
-    fun getAllStation() {
-
-        val listItems: ArrayList<Station> = arrayListOf<Station>()
+    fun getAllStation() : String? {
 
         val request = Request.Builder()
             .url(this.urlStation)
             .build()
 
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {}
-            override fun onResponse(call: Call, response: Response){
+        return client.newCall(request).execute().body()?.string()
 
-                for(item in getAllStationList(response.body()?.string())){
-                    listItems.add(item)
-                }
-
-                listItems.sort()
-
-                context.adapter = StationAdapter(context, listItems)
-
-                context.runOnUiThread(Runnable { context.list?.adapter = context.adapter  })
-            }
-        })
     }
 
     fun getAllStationList(data: String?) : MutableList<Station> {
 
-        val jsonArray: JSONArray = JSONArray(data)
+        val jsonArray = JSONArray(data)
         val count: Int = jsonArray.length()
-        val stations: MutableList<Station> = mutableListOf<Station>()
+        val stations: MutableList<Station> = mutableListOf()
 
         for(i in 1..count){
             val jsonObject: JSONObject = jsonArray.getJSONObject(i-1)
@@ -74,37 +50,19 @@ class APIClient(context: MainActivity){
         return stations
     }
 
-    fun getStationIndex(index: Int) {
+    fun getStationIndex(index: Int): String? {
 
         val request = Request.Builder()
             .url(this.urlStationIndex + index)
             .build()
 
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {}
-            override fun onResponse(call: Call, response: Response){
-
-                var stationIndex: StationIndex = getStationIndexData(response.body()?.string())
-
-                context.runOnUiThread(Runnable {
-                    val builder = AlertDialog.Builder(context)
-                    builder.setTitle("Data: " + stationIndex.Date)
-                    builder.setMessage("Indeks jakości: " + stationIndex.Index)
-                    builder.setIcon(R.drawable.ic_info_outline_black_24dp)
-                    builder.setPositiveButton("OK", DialogInterface.OnClickListener(){
-                        dialog, w ->
-                        dialog.dismiss()
-                    })
-                    builder.show()
-                })
-            }
-        })
+        return client.newCall(request).execute().body()?.string()
 
     }
 
     fun getStationIndexData(data: String?) : StationIndex {
 
-        val jsonObject: JSONObject = JSONObject(data)
+        val jsonObject = JSONObject(data)
         val id = jsonObject.getInt("id")
         val date = jsonObject.getString("stCalcDate")
         val jsonSubObject: JSONObject = jsonObject.getJSONObject("stIndexLevel")
